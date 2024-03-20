4fab1c70eec2e89fc4a1da4e9dffa5f2bdb9eb3d
KoderKavin <kavinalbal01@gmail.com>
2024-03-20 16:35:15 +0530

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,3 +1,4 @@
+import random
 loser = {
         "rock": "paper",
         "paper": "scissor",
@@ -10,30 +11,29 @@ winner = {
     }
 
 
-def who_wins(user_moves,n1,n2):
+def who_wins(user_moves):
     u1 = user_moves[0]
     u2 = user_moves[1]
     if u2 == loser[u1]:
-        return(n2)
+        return("computer")
     elif u1 == loser[u2]:
-        return(n1)
+        return("user")
     else: 
         return("nobody")
     
-def get_move(n1,n2):
-    user1 = input(n1 + " enter your move: ")
-    user2 = input(n2 + " enter your move: ")
+def get_move():
+    user1 = input("enter your move: ")
+    choices = ["rock","paper","scissor"]
+    user2 = random.choice(choices)
+    print("computer plays: " + user2)
     user_moves = [user1,user2]
     return(user_moves)
-
-name1 = input("user1 , enter your name: ")
-name2 = input("user2 , enter your name: ")
-def game(name1,name2):
-    champion = who_wins(get_move(name1,name2),name1,name2)
+def game():
+    champion = who_wins(get_move())
     print(champion + " wins")
     cont = input("do you want to continue? y/n: ")
     if cont == "y":
-        game(name1,name2)
+        game()
 
-game(name1,name2)
+game()
 
